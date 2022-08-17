58d4adb2dd175e61ee38a5853e6b23d64a5110cc
Onur <107554158+On-ur@users.noreply.github.com>
2022-08-17 16:56:19 +0100

    Add files via upload

--- src/test/java/com/qa/pokedexapi/PokedexApiApplicationTests.java
+++ src/test/java/com/qa/pokedexapi/PokedexApiApplicationTests.java
@@ -6,7 +6,6 @@ import com.qa.pokedexapi.Models.Pokemon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.BeforeAll;
-import nl.jqno.equalsverifier.EqualsVerifier;
 
 
 @SpringBootTest
@@ -16,15 +15,5 @@ class PokedexApiApplicationTests {
     void contextLoads() {
     }
 
-    @BeforeAll
-    public static void addPokemon() {
-        testPokemon = new Pokemon("1", "Bulbasaur", "Grass", "Kanto");
-
-    }
-
-    @Test
-    public void testEquals() {
-        EqualsVerifier.simple().forClass(Pokemon.class).verify();
-    }
-
 }
+
