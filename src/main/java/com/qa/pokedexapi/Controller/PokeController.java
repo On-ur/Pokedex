9cbce3cb48b5d18a24db9a544cc9d6d6c2594b6d
Onur <107554158+On-ur@users.noreply.github.com>
2022-08-17 05:21:49 +0100

    Add files via upload

--- src/main/java/com/qa/pokedexapi/Controller/PokeController.java
+++ src/main/java/com/qa/pokedexapi/Controller/PokeController.java
@@ -14,38 +14,45 @@ public class PokeController {
     @Autowired
     private PokeRepo pokeRepo;
 
-    @GetMapping(value ="/")
-    public String getPage(){
-    return "Pokédex";
+    @GetMapping(value = "/")
+    public String getPage() {
+        return "Pokédex";
 
-}
-@GetMapping(value = "/pokemon")
-public List<Pokemon> getPokemon(){
-return pokeRepo.findAll();
+    }
 
-}
+    @GetMapping(value = "/pokemon")
+    public List<Pokemon> getPokemon() {
+        return pokeRepo.findAll();
 
-@PostMapping(value = "/save")
-public String savePokemon(@RequestBody Pokemon pokemon){
-pokeRepo.save(pokemon);
-return "Saved successfully";
+    }
 
-}
-@PutMapping(value = "/update/{id}")
-public String updatePokemon(@PathVariable int id, @RequestBody Pokemon pokemon){
-Pokemon updatedPokemon = pokeRepo.findById(id).get();
-updatedPokemon.setName(pokemon.getName());
-updatedPokemon.setType(pokemon.getType());
-updatedPokemon.setRegion(pokemon.getRegion());
-pokeRepo.save(updatedPokemon);
-return "Updated successfully";
+    @PostMapping(value = "/save")
+    public String savePokemon(@RequestBody Pokemon pokemon) {
+        pokeRepo.save(pokemon);
+        return "Saved successfully";
 
-}
-@DeleteMapping(value = "/delete/{id}")
-public String deletePokemon(@PathVariable int id){
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updatePokemon(@PathVariable int id, @RequestBody Pokemon pokemon) {
+        Pokemon updatedPokemon = pokeRepo.findById(id).get();
+        updatedPokemon.setName(pokemon.getName());
+        updatedPokemon.setType(pokemon.getType());
+        updatedPokemon.setRegion(pokemon.getRegion());
+        pokeRepo.save(updatedPokemon);
+        return "Updated successfully";
+
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deletePokemon(@PathVariable int id) {
         Pokemon deletePokemon = pokeRepo.findById(id).get();
         pokeRepo.delete(deletePokemon);
-        return "Deleted Pokemon with the ID:"+id;
-}
+        return "Deleted Pokemon with the ID:" + id;
+    }
 
+    @GetMapping(value = "/getPokemon/{id}")
+    public Pokemon getPokemonById(@PathVariable int id) {
+        return pokeRepo.findById(id).get();
+    }
 }
