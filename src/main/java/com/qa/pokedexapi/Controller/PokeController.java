ce5bb19caa65bbf0fdf1a813eaf00cabb63617c8
Onur <107554158+On-ur@users.noreply.github.com>
2022-08-17 15:30:10 +0100

    Add files via upload

--- src/main/java/com/qa/pokedexapi/Controller/PokeController.java
+++ src/main/java/com/qa/pokedexapi/Controller/PokeController.java
@@ -3,7 +3,10 @@ package com.qa.pokedexapi.Controller;
 import com.qa.pokedexapi.Models.Pokemon;
 import com.qa.pokedexapi.Repo.PokeRepo;
 
+import com.sun.net.httpserver.HttpsParameters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,20 +22,20 @@ public class PokeController {
         return "Pokédex";
 
     }
-
+    @CrossOrigin
     @GetMapping(value = "/pokemon")
     public List<Pokemon> getPokemon() {
         return pokeRepo.findAll();
 
     }
-
+    @CrossOrigin
     @PostMapping(value = "/save")
     public String savePokemon(@RequestBody Pokemon pokemon) {
         pokeRepo.save(pokemon);
         return "Saved successfully";
 
     }
-
+    @CrossOrigin
     @PutMapping(value = "/update/{id}")
     public String updatePokemon(@PathVariable int id, @RequestBody Pokemon pokemon) {
         Pokemon updatedPokemon = pokeRepo.findById(id).get();
@@ -43,16 +46,17 @@ public class PokeController {
         return "Updated successfully";
 
     }
-
+    @CrossOrigin
     @DeleteMapping(value = "/delete/{id}")
     public String deletePokemon(@PathVariable int id) {
         Pokemon deletePokemon = pokeRepo.findById(id).get();
         pokeRepo.delete(deletePokemon);
         return "Deleted Pokemon with the ID:" + id;
     }
-
+    @CrossOrigin
     @GetMapping(value = "/getPokemon/{id}")
     public Pokemon getPokemonById(@PathVariable int id) {
         return pokeRepo.findById(id).get();
     }
+
 }
